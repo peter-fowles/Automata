acd3ed46cab5b174698413ce662ffc32b4ec9151
fowlinbasket <foul.in.basket@gmail.com>
2024-09-30 15:53:28 -0600

    Moved classes to be nested

--- stackmachine.py
+++ stackmachine.py
@@ -31,73 +31,7 @@ class ContextFreeGrammar:
     def produces(self, string):
         pass
 
-class Operation:
-    def __init__(self, read, pop, push):
-        self.read = read
-        self.pop = pop
-        self.push = push
 
-    def __str__(self) -> str:
-        return f'read {self.read}, pop {self.pop}, push {self.push}'
-        
-    def __repr__(self):
-        return f'read {self.read}, pop {self.pop}, push {self.push}'
-
-class Stack:
-    def __init__(self):
-        self.__stack = []
-
-    def show(self):
-        return self.__stack
-    
-    def push(self, item):
-        if item is not None:
-            self.__stack.append(item)
-
-    def pop(self):
-        return self.__stack.pop()
-    
-    def copy(self):
-        newStack = Stack()
-        for item in self.__stack:
-            newStack.push(item)
-        return newStack
-    
-    def top(self):
-        if len(self.__stack) == 0:
-            return None
-        return self.__stack[-1]
-    
-    def isEmpty(self):
-        return len(self.__stack) == 0
-    
-    def clear(self):
-        self.__stack = []
-    
-    def __eq__(self, other: object) -> bool:
-        if len(self.__stack) != len(other.__stack):
-            return False
-        result = True
-        for i in range(len(self.__stack)):
-            result &= self.__stack[i] == other.__stack[i]
-        return result
-
-    def __str__(self):
-        return str(self.__stack) + ', top = ' + str(self.top())
-    
-class Snapshot:
-    def __init__(self, string, stack):
-        self.string = string
-        self.stack = stack
-    
-    def __eq__(self, other: object) -> bool:
-        return self.string == other.string and self.stack == other.stack
-    
-    def __str__(self):
-        return f'{self.string}, {self.stack}'
-    
-    def __repr__(self) -> str:
-        return self.__str__()
 
 class StackMachine:
     def __init__(self, G):
@@ -107,20 +41,14 @@ class StackMachine:
         for v in self.grammar.variables:
             if v in self.grammar.productions:
                 for production in self.grammar.productions[v]:
-                    self.operations.add(Operation(None, v, production))
+                    self.operations.add(self.Operation(None, v, production))
         for a in self.grammar.alphabet:
-            self.operations.add(Operation(a, a, None))
-    
-    def __repr__(self) -> str:
-        s = ''
-        for op in self.operations:
-            s += f'{op}\n'
-        return s
+            self.operations.add(self.Operation(a, a, None))
     
     def process(self, string):
-        stack = Stack()
+        stack = self.Stack()
         stack.push(self.grammar.start_symbol)
-        return self.__process(Snapshot(string, stack))
+        return self.__process(self.Snapshot(string, stack))
 
     def __process(self, start):
         queue = [start]
@@ -138,12 +66,86 @@ class StackMachine:
                             nextStack.push(item)
                     if op.read is not None:
                         nextString = curr.string[1:]
-                    nextState = Snapshot(nextString, nextStack)
+                    nextState = self.Snapshot(nextString, nextStack)
                     if nextState not in seenStates:
                         queue.append(nextState)
             if len(curr.string) == 0 and curr.stack.isEmpty():
                 return True
         return False
+    
+    def __repr__(self) -> str:
+        s = ''
+        for op in self.operations:
+            s += f'{op}\n'
+        return s
+
+    class Operation:
+        def __init__(self, read, pop, push):
+            self.read = read
+            self.pop = pop
+            self.push = push
+
+        def __str__(self) -> str:
+            return f'read {self.read}, pop {self.pop}, push {self.push}'
+            
+        def __repr__(self):
+            return f'read {self.read}, pop {self.pop}, push {self.push}'
+
+    class Stack:
+        def __init__(self):
+            self.__stack = []
+
+        def show(self):
+            return self.__stack
+        
+        def push(self, item):
+            if item is not None:
+                self.__stack.append(item)
+
+        def pop(self):
+            return self.__stack.pop()
+        
+        def copy(self):
+            newStack = type(self)()
+            for item in self.__stack:
+                newStack.push(item)
+            return newStack
+        
+        def top(self):
+            if len(self.__stack) == 0:
+                return None
+            return self.__stack[-1]
+        
+        def isEmpty(self):
+            return len(self.__stack) == 0
+        
+        def clear(self):
+            self.__stack = []
+        
+        def __eq__(self, other: object) -> bool:
+            if len(self.__stack) != len(other.__stack):
+                return False
+            result = True
+            for i in range(len(self.__stack)):
+                result &= self.__stack[i] == other.__stack[i]
+            return result
+
+        def __str__(self):
+            return str(self.__stack) + ', top = ' + str(self.top())
+    
+    class Snapshot:
+        def __init__(self, string, stack):
+            self.string = string
+            self.stack = stack
+        
+        def __eq__(self, other: object) -> bool:
+            return self.string == other.string and self.stack == other.stack
+        
+        def __str__(self):
+            return f'{self.string}, {self.stack}'
+        
+        def __repr__(self) -> str:
+            return self.__str__()
             
         
 if __name__ == '__main__':
@@ -152,7 +154,7 @@ if __name__ == '__main__':
     G.addProduction('S', 'bSb')
     G.addProduction('S', None)
     M = StackMachine(G)
-    print(M.process('abaaba'))
+    print(M.process('abba'))
 
             
         
